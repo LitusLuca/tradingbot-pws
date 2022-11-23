71a5b05e08d530e7ff57d2ffc352b7774b2060b1
Greatbator <113986712+Greatbator@users.noreply.github.com>
2022-11-23 17:55:00 +0100

    taalfoutjes

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from datetime import date
 
 from Agent import TradingAgent
-from TrainingEnviroment import StockSimulation
+from TrainingEnvironment import StockSimulation
 
 
-def SimpleTraining(instrument ,episodes, timeSpan, epidsodeStartDate, iteration):
-    enviroment = StockSimulation(instrument, timeSpan, epidsodeStartDate, results="./results/simple-{}-{}.xlsx".format(instrument, iteration))
-    agent = TradingAgent(enviroment.inputSpace, enviroment.actionSpace, safeFile="./models/simple-{}-{}.h5".format(instrument, iteration), epsilonDeqay=0.99)
+def SimpleTraining(instrument ,episodes, timeSpan, episodeStartDate, iteration):
+    environment = StockSimulation(instrument, timeSpan, episodeStartDate, results="./results/simple-{}-{}.xlsx".format(instrument, iteration))
+    agent = TradingAgent(environment.inputSpace, environment.actionSpace, safeFile="./models/simple-{}-{}.h5".format(instrument, iteration), epsilonDeqay=0.99)
     agent.load()
 
     for E in range(episodes):
         print("start episode: {:d}/{:d}, exploration: {:.2f}".format(E+1, episodes, agent.exploration))
         
-        state = enviroment.getState()
+        state = environment.getState()
         print(state)
-        for t in range(enviroment.getEpisodeLength()):
+        for t in range(environment.getEpisodeLength()):
             action = agent.predictAction(state)
-            nextState, reward, finished = enviroment.action(action)
+            nextState, reward, finished = environment.action(action)
             agent.remember(state, action, reward, nextState, finished)
             state = nextState
             if finished:
@@ -24,8 +24,8 @@ def SimpleTraining(instrument ,episodes, timeSpan, epidsodeStartDate, iteration)
             agent.trainMemories(10)
             if t % 50 == 0:
                 agent.save()
-        print("total profit: {:.2f}".format(enviroment.profit))
-        enviroment.reset()
+        print("total profit: {:.2f}".format(environment.profit))
+        environment.reset()
 
 
 
