4e5e18fde7b8935faf240e36869592706dc3cafe
LitusLuca <76185482+LitusLuca@users.noreply.github.com>
2022-12-02 15:05:16 +0100

    upload laatste resultaten naar github

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def randomDate(start: date, end: date):
 def ChangingTraining(instrument, episodes, episodeTimeSpan, minDate, maxDate, iteration):
     episodeStartDate = randomDate(minDate, maxDate)
     environment = StockSimulation(instrument, episodeTimeSpan, episodeStartDate, results="./results/changing-{}-{}.xlsx".format(instrument, iteration))
-    agent = TradingAgent(environment.inputSpace, environment.actionSpace, safeFile="./models/changing-{}-{}.h5".format(instrument, iteration), epsilonDecay=0.97, gamma=0.9, epsilonMinimum=0.1)
+    agent = TradingAgent(environment.inputSpace, environment.actionSpace, safeFile="./models/changing-{}-{}.h5".format(instrument, iteration), epsilonDecay=0.9, gamma=0.9, epsilonMinimum=0.1, epsilon=0.5)
     agent.load()
     for E in range(episodes):
         print("start episode: {:d}/{:d}, exploration: {:.2f}".format(E+1, episodes, agent.exploration))
@@ -65,5 +65,5 @@ def ChangingTraining(instrument, episodes, episodeTimeSpan, minDate, maxDate, it
         environment.startDate = episodeStartDate
 
 if __name__ == "__main__":
-    ChangingTraining("meta", 1000, 365, date(2013, 1, 1), date(2021, 11, 1), 4.1)
+    ChangingTraining("microsoft", 1000, 365, date(2013, 1, 1), date(2021, 11, 1), 3.0)
     #SimpleTraining("microsoft", 100, 400, date(2018, 1, 1), 5)
